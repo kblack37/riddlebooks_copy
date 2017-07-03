f1ba93d0ae91585143f92f320f93d2bcffc9eeca
Roy Szeto <szetor@cs.washington.edu>
2017-07-03 09:12:11 -0700

    Added project files for ide

---
+++ haxe/src/TestMain.hx
@@ -0,0 +1,22 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author 
+ */
+class TestMain extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+		trace("Hello World");
+	}
+
+}
